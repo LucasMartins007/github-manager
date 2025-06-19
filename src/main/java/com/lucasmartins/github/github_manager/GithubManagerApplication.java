cbd46a37e3dffdd3b265032162b1ca3abb892aa6
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2025-06-19 16:48:25 -0300

    feat: adaptando para arquitetura hexagonal - concluindo endpoint para consulta de PRs e commits atrasados

---
+++ src/main/java/com/lucasmartins/github/github_manager/GithubManagerApplication.java
@@ -0,0 +1,15 @@
+package com.lucasmartins.github.github_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class GithubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubManagerApplication.class, args);
+	}
+
+}
